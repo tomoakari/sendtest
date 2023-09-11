40701816323b8199fc311391c657ef3e6600b1cb
tomoyuki matsumoto <matsumoto.tomoyuki@valuesccg.com>
2023-09-11 14:57:24 +0900

    hackasontest

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,40 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <button v-on:click="sendPost()">SEND!!!</button>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+const axiosBase = require('axios');
+const axios = axiosBase.create({
+  baseURL: 'https://asia-northeast1-hackathon-202309.cloudfunctions.net', 
+  headers: {
+    'Content-Type': 'application/json',
+    'X-Requested-With': 'XMLHttpRequest'
+  },
+  responseType: 'json'  
+});
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+  },
+  methods: {
+    sendPost(){
+      axios.post('/store-input-customer-info', {
+        "family_name": "鈴木",
+        "first_name": "太郎",
+        "customer_name": "丸メガネをかけた男性",
+        "sex": "男性",
+        "age_bin": "30歳-35歳",
+        "customer_line_id": "abc123",
+        "customer_id_external": "ext001"
+      })
+      .then(function (response) {
+        console.log(response.data);
+      })
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
